260ea488c3cb3a2be8a9cc2936f85242b6cf6833
arnavg0418 <arnavg0418@gmail.com>
2025-09-01 16:59:52 +0530

    made src a package

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# src/__init__.py
+
+"""
+This is the initialization file for the `src` package.
+You can use this file to initialize the package, import key modules, or define package-level variables.
+"""
+
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
